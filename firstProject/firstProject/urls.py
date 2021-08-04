6ae8561ad325f132c7af5c54e651e54b1dd73378
Liberbell <liberbell@gmail.com>
2021-08-05 08:57:34 +0900

    urls6

--- firstProject/firstProject/urls.py
+++ firstProject/firstProject/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('', views.homepage),
+    path('home/', views.homepage),
     path('admin/', admin.site.urls),
 ]
