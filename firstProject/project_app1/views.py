79bea220eb0b0f0e007b8181b32c5a1b56c765f8
Liberbell <liberbell@gmail.com>
2021-08-05 09:15:35 +0900

    views2

--- firstProject/project_app1/views.py
+++ firstProject/project_app1/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def app_homepage(request):
+    return HttpResponse("This is the firstproject App.")
