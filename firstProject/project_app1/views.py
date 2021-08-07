82c901b9f03ae3bd03a3821c3e88a3ae4f0d3da2
Liberbell <liberbell@gmail.com>
2021-08-07 15:15:11 +0900

    views1

--- firstProject/project_app1/views.py
+++ firstProject/project_app1/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse
 
 def app_homepage(request):
     return render(request, "homepage.html")
+
+def about_us(request):
+    return render(request, "aboutus.html")
+
+def services(request):
+    return render(request, "services.html")
+
+def contact_us(request):
+    return render(request, "contactus.html")
