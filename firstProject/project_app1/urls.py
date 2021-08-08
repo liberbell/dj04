25b80fef368a9f6288a50debafd0ed5b1a31c795
Liberbell <liberbell@gmail.com>
2021-08-08 13:01:57 +0900

    urls1

--- firstProject/project_app1/urls.py
+++ firstProject/project_app1/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path, include
 
 urlpatterns = [
-    path("", views.app_homepage, name="app_homepage")
+    path("", views.app_homepage, name="app_homepage"),
+    path("about_us", views.about_us, name="about_us")
 ]
