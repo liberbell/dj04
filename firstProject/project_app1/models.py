a306c211119a795d4c8818e95718a0744b8fb10a
Liberbell <liberbell@gmail.com>
2021-08-08 13:44:22 +0900

    models2

--- firstProject/project_app1/models.py
+++ firstProject/project_app1/models.py
@@ -4,3 +4,5 @@ from django.db import models
 
 class RegisteredUser(models.Model):
     name = models.CharField(max_length=100)
+    emailid = models.CharField(max_length=100)
+    phoneNum = models.CharField(brank=True, null=True, max_length=20)
