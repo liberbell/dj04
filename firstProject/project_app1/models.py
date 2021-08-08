67e6ebab9fa811d7305e6ebe43fceff2d6656757
Liberbell <liberbell@gmail.com>
2021-08-08 13:44:49 +0900

    models3

--- firstProject/project_app1/models.py
+++ firstProject/project_app1/models.py
@@ -6,3 +6,4 @@ class RegisteredUser(models.Model):
     name = models.CharField(max_length=100)
     emailid = models.CharField(max_length=100)
     phoneNum = models.CharField(brank=True, null=True, max_length=20)
+    password = models.CharField(max_length=30)
