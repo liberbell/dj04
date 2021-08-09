2915905c2ffbcc381a230ad23642bb507ae374da
Liberbell <liberbell@gmail.com>
2021-08-09 13:10:33 +0900

    views1

--- firstProject/project_app1/views.py
+++ firstProject/project_app1/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+from django.contrib import messages
 from django.http import HttpResponse
 
 # Create your views here.
