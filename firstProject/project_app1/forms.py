6e77619eb7e27d4b8502f0586fc9940ce2968c0d
Liberbell <liberbell@gmail.com>
2021-08-09 13:04:12 +0900

    forms1

--- firstProject/project_app1/forms.py
+++ firstProject/project_app1/forms.py
@@ -1,3 +1,11 @@
-from django.forms import ModelForm
+from django.db import models
+from django.forms import ModelForm, fields
 from django import forms
 from .models import RegisteredUser
+
+class RegisterForm(ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = RegisteredUser
+        fields = '__all__'
