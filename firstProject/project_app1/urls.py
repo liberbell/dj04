973af7e20c92299594c2179fbd5800311c1caf7e
Liberbell <liberbell@gmail.com>
2021-08-09 13:25:08 +0900

    urls1

--- firstProject/project_app1/urls.py
+++ firstProject/project_app1/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.app_homepage, name="app_homepage"),
     path("about_us", views.about_us, name="about_us"),
     path("services", views.services, name="services"),
-    path("contact_us", views.contact_us, name="contact_us")
+    path("contact_us", views.contact_us, name="contact_us"),
+    path("register", views.register, name="register")
 ]
