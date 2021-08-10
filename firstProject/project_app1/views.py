dfd3ea243575e5d130d5c25d73817668c0c8ee8d
Liberbell <liberbell@gmail.com>
2021-08-11 08:53:11 +0900

    views4

--- firstProject/project_app1/views.py
+++ firstProject/project_app1/views.py
@@ -41,7 +41,6 @@ def signin(request):
 
         try:
             user = RegisteredUser.objects.get(name=usrnme)
-            print(user)
             if usrnme == user.name and passwd == user.password:
                 return redirect("loggedin")
             
