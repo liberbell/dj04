f6f09c8015a10b6245a99c2e4d4658f9c73ab93b
Liberbell <liberbell@gmail.com>
2021-08-18 08:36:36 +0900

    views1

--- firstProject/products_app/views.py
+++ firstProject/products_app/views.py
@@ -4,9 +4,11 @@ from django.shortcuts import render, redirect
 # Create your views here.
 
 def product_list(reuqest):
-    products = {"diary": ["milk", "yougurt", "cheese", "butter"],
+    products = {
+                "dairy": ["milk", "yougurt", "cheese", "butter"],
                 "stones": ["rock", "stones", "leva", "molten rock"],
                 "tumbleweed": ["weed", "grass", "dead grass"],
-                "air": ["smog", "smoke", "atomosphere"]}
+                "air": ["smog", "smoke", "atomosphere"]
+                }
 
     return render(request, "products_list.html", products)
