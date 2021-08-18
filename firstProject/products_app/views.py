f3a22b8321d225a6ce888f8d12dbfd19ab46669b
Liberbell <liberbell@gmail.com>
2021-08-18 09:37:07 +0900

    views6

--- firstProject/products_app/views.py
+++ firstProject/products_app/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, redirect
-from django.http import request
 
 
-def product_list(reuqest):
+
+def product_list(request):
     products = {
-                "dairy": ["milk", "yougurt", "cheese", "butter"]
-                # "stones": ["rock", "stones", "leva", "molten rock"],
-                # "tumbleweed": ["weed", "grass", "dead grass"],
-                # "air": ["smog", "smoke", "atomosphere"]
+                "dairy": ["milk", "yougurt", "cheese", "butter"],
+                "stones": ["rock", "stones", "leva", "molten rock"],
+                "tumbleweed": ["weed", "grass", "dead grass"],
+                "air": ["smog", "smoke", "atomosphere"]
                 }
 
     return render(request, "products_list.html", products)
