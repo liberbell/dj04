f84586ad3ee912c11be31a9747102f187d0fa839
Liberbell <liberbell@gmail.com>
2021-08-18 09:34:06 +0900

    models1

--- firstProject/products_app/models.py
+++ firstProject/products_app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class OrderList(models.Model):
+    username = models.CharField(max_length=100)
+    wholelist = models.CharField(max_length=500, blank=True, null=True)
