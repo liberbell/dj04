fcb875452cae1b7f5ade75a34d746db95f14ef96
Liberbell <liberbell@gmail.com>
2021-08-19 09:19:10 +0900

    urls7

--- firstProject/project_app1/urls.py
+++ firstProject/project_app1/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path("userupdate/<int:pk>/", views.UserUpdateView.as_view(template_name="user_create.html"), name="userupdate"),
     path("userdelete/<int:pk>/", views.UserDeleteView.as_view(template_name="user_confirm_delete.html"), name="userdelete"),
     path("products/", prod_views.product_list, name="products_list"),
-    path("products/order/", prod_views.order, name="order"),
+    path("order/", prod_views.order, name="order"),
 ]
 
 if settings.DEBUG:
