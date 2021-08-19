f02523649060907f73d505bcc3050b6c8abf3fa0
Liberbell <liberbell@gmail.com>
2021-08-20 08:54:49 +0900

    views2

--- firstProject/products_app/views.py
+++ firstProject/products_app/views.py
@@ -8,7 +8,7 @@ def product_list(request):
                 "dairy": ["milk", "yougurt", "cheese", "butter"],
                 "stones": ["rock", "stones", "leva", "molten rock"],
                 "tumbleweed": ["weed", "grass", "dead grass"],
-                "air": ["smog", "smoke", "atomosphere"]
+                "air": ["smog", "smoke", "atomosphere"],
                 }
 
     return render(request, "products_list.html", products)
