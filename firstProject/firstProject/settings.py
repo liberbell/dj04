3ef740eacd2e62150abbfc5d5a08f6bce435c029
Liberbell <liberbell@gmail.com>
2021-08-20 09:11:28 +0900

    settins2

--- firstProject/firstProject/settings.py
+++ firstProject/firstProject/settings.py
@@ -129,3 +129,5 @@ EMAIL_BACKEND = 'django.core.email.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
+EMAIL_HOST_USER = "alex@examle.com"
+EMAIL_HOST_PASSWORD = "verysecret"
